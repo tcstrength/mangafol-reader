39d26b0abddcfe3499e081af56dfc22841a014be
tcstrength <thai.cuong1404@gmail.com>
2021-07-31 05:52:47 +0700

    prevent html

--- src/utils/UrlUtils.js
+++ src/utils/UrlUtils.js
@@ -1,8 +1,9 @@
 export const urlify = (text) => {
   var urlRegex = /(https?:\/\/[^\s]+)/g;
+  var regX = /(<([^>]+)>)/ig;
+  text = text.replace(regX, "");
+
   return text.replace(urlRegex, function (url) {
     return '<a target="_blank" href="' + url + '">' + url + '</a>';
   })
-  // or alternatively
-  // return text.replace(urlRegex, '<a href="$1">$1</a>')
 }
